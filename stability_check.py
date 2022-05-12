8c8b0d2aa82625e38b1e0536f8f558c551a7ac8a
Manuel Nowack <manuel.nowack@inf.ethz.ch>
2022-05-12 11:08:50 +0200

    Rewrite stability check

--- stability_check.py
+++ stability_check.py
@@ -1,3 +1,4 @@
+import datetime
 import numpy as np
 import os
 import sys
@@ -5,27 +6,22 @@ import sys
 import benchmark
 
 
-def print_noise(run_times: np.ndarray):
-    reference_time = np.median(run_times)
-    noise = np.abs(1 - run_times / reference_time)
-    for x in noise:
-        print("{:.2f} %".format(x * 100), file=log)
-    max_noise = 1.0 - run_times.min() / run_times.max()
-    print("Max noise: {:.2f} %".format(max_noise * 100), file=log)
-    std = np.sqrt(np.mean((np.abs(run_times - reference_time) ** 2)))
-    print("Standard deviation (median):", std, file=log)
-
-
-log = open("results/{:s}.log".format(os.path.basename(__file__)), "w")
-
 try:
     repetitions = int(sys.argv[1])
 except IndexError:
     repetitions = 10
 run_times = np.array([benchmark.run("") for _ in range(repetitions)])
-with open("results/stability.txt", "a") as fh:
+# Write runtimes to file for later usage
+timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
+with open(f"results/stability_{timestamp}.txt", "x") as fh:
     np.savetxt(fh, run_times)
-print("=== Current benchmark ===", file=log)
-print_noise(run_times)
-print("=== All benchmarks ===", file=log)
-print_noise(np.loadtxt("results/stability.txt"))
+# Log noise for your information
+with open(f"results/{os.path.basename(__file__)}.log", "w") as fh:
+    reference_time = np.median(run_times)
+    noise = np.abs(1 - run_times / reference_time)
+    for x in noise:
+        fh.write(f"{x * 100:.2f} %\n")
+    max_noise = 1.0 - run_times.min() / run_times.max()
+    fh.write(f"Max noise: {max_noise * 100:.2f} %\n")
+    std = np.sqrt(np.mean((np.abs(run_times - reference_time) ** 2)))
+    fh.write(f"Standard deviation (from median) {std}:\n")
