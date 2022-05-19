96eee3126e542699cb925fe43a34e351a338eb3e
Manuel Nowack <manuel.nowack@inf.ethz.ch>
2022-05-19 08:48:03 +0200

    Use relative standard deviation

--- stability_check.py
+++ stability_check.py
@@ -32,5 +32,6 @@ with open(f"results/{os.path.basename(__file__)}.log", "w") as fh:
         fh.write(f"{x * 100:.2f} %\n")
     max_noise = 1.0 - run_times.min() / run_times.max()
     fh.write(f"Max noise: {max_noise * 100:.2f} %\n")
-    std = np.sqrt(np.mean((np.abs(run_times - reference_time) ** 2)))
-    fh.write(f"Standard deviation (from median) {std}:\n")
+    # std = np.sqrt(np.mean((np.abs(run_times - reference_time) ** 2)))
+    std = np.std(run_times) / np.mean(run_times)
+    fh.write(f"Relative standard deviation {std}:\n")
