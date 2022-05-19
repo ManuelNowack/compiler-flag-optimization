09dc0973fbb3206e38cdb5d1227026c098250d05
Manuel Nowack <manuel.nowack@inf.ethz.ch>
2022-05-19 10:13:22 +0200

    Expand command line arguments

--- stability_check.py
+++ stability_check.py
@@ -1,37 +1,33 @@
+import argparse
+import benchmark
 import datetime
 import numpy as np
-import os
-import sys
-
-import benchmark
 
 
-try:
-    repetitions = int(sys.argv[1])
-except IndexError:
-    repetitions = 10
-try:
-    program = sys.argv[2]
-except IndexError:
-    program = "cbench-network-dijkstra"
-try:
-    dataset = sys.argv[3]
-except IndexError:
-    dataset = "cdataset-dijkstra-0001"
-run_times = np.array([benchmark.run("", program, dataset)
-                     for _ in range(repetitions)])
-# Write runtimes to file for later usage
+class SplitArgs(argparse.Action):
+    def __call__(self, parser, namespace, values, option_string=None):
+        setattr(namespace, self.dest, values.split(","))
+
+
+default_programs = ["cbench-network-dijkstra"]
+default_datasets = ["cdataset-dijkstra-0001"]
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--repetitions", default=10, type=int)
+parser.add_argument("--program", default=default_programs, action=SplitArgs)
+parser.add_argument("--dataset", default=default_datasets, action=SplitArgs)
+args = parser.parse_args()
+
 timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
-with open(f"results/{program}_{timestamp}.txt", "x") as fh:
-    np.savetxt(fh, run_times)
-# Log noise for your information
-with open(f"results/{os.path.basename(__file__)}.log", "w") as fh:
-    reference_time = np.median(run_times)
-    noise = np.abs(1 - run_times / reference_time)
-    for x in noise:
-        fh.write(f"{x * 100:.2f} %\n")
-    max_noise = 1.0 - run_times.min() / run_times.max()
-    fh.write(f"Max noise: {max_noise * 100:.2f} %\n")
-    # std = np.sqrt(np.mean((np.abs(run_times - reference_time) ** 2)))
-    std = np.std(run_times) / np.mean(run_times)
-    fh.write(f"Relative standard deviation {std}:\n")
+with open(f"results/stability_check.txt", "w") as fh:
+    for program, dataset in zip(args.program, args.dataset):
+        run_times = np.array([benchmark.run("", program, dataset)
+                             for _ in range(args.repetitions)])
+        # Write runtimes to file for later usage
+        np.savetxt(f"results/{program}_{timestamp}.txt", run_times)
+        # Log noise for your information
+        fh.write(f"{program}\n")
+        max_noise = 1.0 - run_times.min() / run_times.max()
+        fh.write(f"Max noise: {max_noise * 100:.2f} %\n")
+        std = np.std(run_times) / np.mean(run_times)
+        fh.write(f"Relative standard deviation {std}\n")
