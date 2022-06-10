1a28c1cd82c4487020e275aeaba666ed06252eb4
Manuel Nowack <manuel.nowack@inf.ethz.ch>
2022-06-10 16:52:18 +0200

    Do not look at assembly during normal compilation

--- benchmark.py
+++ benchmark.py
@@ -1,7 +1,5 @@
 import ck.kernel as ck
-import glob
 import os
-from tuner.flag_info import extract_gcc_flags
 
 
 def ck_cmd(cmd: dict):
@@ -19,18 +17,8 @@ def compile(program: str, flags: str, lflags: str = "",
                 "module_uoa": "program",
                 "data_uoa": program,
                 "generate_rnd_tmp_dir": "yes" if generate_rnd_tmp_dir else "",
-                "flags": flags + " -save-temps -fverbose-asm",
+                "flags": flags,
                 "lflags": lflags})
-    expected_flags = {f for f in flags.split()
-                      if f.startswith("-f") and not f.startswith("-fno-")}
-    for file in glob.glob(os.path.join(r["tmp_dir"], "*.s")):
-        actual_flags = set(extract_gcc_flags(file))
-        missing_flags = expected_flags - actual_flags
-        unexpected_flags = actual_flags - expected_flags
-        if missing_flags:
-            print("Missing flags", sorted(missing_flags))
-        if unexpected_flags:
-            print("Unexpected flags", sorted(unexpected_flags))
     return r["tmp_dir"]
 
 
