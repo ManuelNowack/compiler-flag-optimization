3f3675ddf76cc80ed99121a9b882f030723f9a4b
Manuel Nowack <manuel.nowack@inf.ethz.ch>
2022-06-11 12:32:39 +0200

    Remove redundant line

--- tuner/base_tuner.py
+++ tuner/base_tuner.py
@@ -6,6 +6,5 @@ class Tuner:
         self.default_optimization = default_optimization
         self.default_perf = evaluator.evaluate(default_optimization, 10)
 
-    
     def tune(self, budget, batch_size=1, file=None):
         raise NotImplementedError
