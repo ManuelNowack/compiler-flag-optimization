4b6797adf220eecb3d586e443b1c473075880cd8
Manuel Nowack <manuel.nowack@inf.ethz.ch>
2022-06-18 18:25:26 +0200

    Generate random samples

--- compiler_opt/__init__.py
+++ compiler_opt/__init__.py
@@ -6,5 +6,6 @@ from .experiment import Experiment
 from .flag_info import optimization_to_str, read_gcc_search_space
 from .fourier_tuner import FourierTuner
 from .mono_tuner import MonoTuner
+from .samples import Samples
 from .random_tuner import RandomTuner
 from .srtuner import SRTuner
---
+++ compiler_opt/samples.py
@@ -0,0 +1,54 @@
+import multiprocessing
+import random
+
+import numpy as np
+import pandas as pd
+
+from . import Evaluator
+from . import flag_info
+from .typing import Optimization
+
+
+class Samples():
+    def __init__(
+            self,
+            programs: tuple[str, str, str],
+            samples: int,
+            parallel: int = None):
+        self.programs = programs
+        self.samples = samples
+        self.parallel = parallel
+        self.search_space = flag_info.read_gcc_search_space("gcc_opts.txt")
+        self.rng = random.Random(42)
+        self.optimizations = [self.random_optimization_()
+                              for _ in range(self.samples)]
+        self.run_()
+        self.write_results_()
+
+    def random_optimization_(self) -> Optimization:
+        optimization = {}
+        for flag_name, configs in self.search_space.items():
+            optimization[flag_name] = self.rng.choice(configs)
+        return optimization
+
+    def sample_thread_(self, program: str, dataset: str,
+                       command: str) -> list[float]:
+        evaluator = Evaluator(program, 1, self.search_space, dataset, command)
+        return [evaluator.evaluate(opt) for opt in self.optimizations]
+
+    def run_(self) -> None:
+        if self.parallel is not None:
+            with multiprocessing.Pool(processes=self.parallel) as pool:
+                self.results = pool.starmap(self.sample_thread_, self.programs)
+        else:
+            self.results = [self.sample_thread_(program, dataset, command)
+                            for program, dataset, command in self.programs]
+
+    def write_results_(self) -> None:
+        program_names = [program if command == "" else f"{program}-{command}"
+                         for program, _, command in self.programs]
+        flags = [flag_info.optimization_to_str(opt, self.search_space)
+                 for opt in self.optimizations]
+        data = np.array(self.results).transpose()
+        df = pd.DataFrame(data=data, index=flags, columns=program_names)
+        df.to_csv(f"samples/{self.samples}.csv")
