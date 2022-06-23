4e13a723a89ebd83681ba00ee3d29efae4001231
Manuel Nowack <manuel.nowack@inf.ethz.ch>
2022-06-23 16:27:38 +0200

    Refactor mono tuner

--- compiler_opt/mono_tuner.py
+++ compiler_opt/mono_tuner.py
@@ -1,3 +1,4 @@
+import operator
 from typing import TextIO
 
 from . import base_tuner
@@ -13,48 +14,61 @@ class MonoTuner(base_tuner.Tuner):
             evaluator: evaluator.Evaluator,
             default_optimization: Optimization):
         super().__init__(search_space, evaluator, "MonoTuner", default_optimization)
-        self.default_flags = flag_info.read_gcc_flags(
+
+    def mutate_optimization_by_individual_flags(
+            self, optimization: Optimization) -> list[tuple[Optimization, float]]:
+        base_flags = flag_info.read_gcc_flags(
             self.evaluator.program, flag_info.optimization_to_str(
-                self.default_optimization, self.search_space))
-
-    def search_space_size(self) -> int:
-        size = 0
-        for flag_name, domain in self.search_space.items():
-            if flag_name == "stdOptLv":
-                continue
-            size += len(domain)
-        return size
-
-    def find_best_optimization(
-            self,
-            budget: int,
-            file: TextIO = None) -> Optimization:
-        repeats = budget // self.search_space_size()
-        assert repeats > 0
-
-        best_optimization = self.default_optimization
-        best_runtime = float("inf")
+                optimization, self.search_space))
+        mutated_optimizations = []
         for flag_name, domain in self.search_space.items():
             if flag_name == "stdOptLv":
                 continue
             for value in domain:
                 if value is True:
-                    if flag_name in self.default_flags:
+                    if flag_name in base_flags:
                         continue
                 elif value is False:
-                    if flag_name not in self.default_flags:
+                    if flag_name not in base_flags:
                         continue
                 else:
                     flag = f"{flag_name}={value}"
-                    if flag in self.default_flags:
+                    if flag in base_flags:
                         continue
-                curr_optimization = self.default_optimization.copy()
-                curr_optimization[flag_name] = value
-                curr_perf = self.evaluator.evaluate(curr_optimization, repeats)
-                if file is not None:
-                    speedup = self.default_perf / curr_perf
-                    file.write(f"{flag_name}: {speedup:.3f}\n")
-                if best_runtime > curr_perf:
-                    best_runtime = curr_perf
-                    best_optimization = curr_optimization
+                new_optimization = optimization.copy()
+                new_optimization[flag_name] = value
+                run_time = self.evaluator.evaluate(new_optimization)
+                mutated_optimizations.append((new_optimization, run_time))
+        mutated_optimizations.sort(key=operator.itemgetter(1), reverse=True)
+        return mutated_optimizations
+
+    def find_best_optimization(
+            self,
+            budget: int,
+            file: TextIO = None) -> Optimization:
+        best_optimization = self.default_optimization
+        best_runtime = self.evaluator.evaluate(best_optimization)
+        while budget > 0:
+            candidates = self.mutate_optimization_by_individual_flags(
+                best_optimization)
+            budget -= len(candidates)
+            if budget < 0:
+                del candidates[budget:]
+            next_optimization = best_optimization.copy()
+            for optimization, run_time in candidates[:1]:
+                if run_time > best_runtime:
+                    curr_optimization_set = set(optimization.items())
+                    best_optimization_set = set(best_optimization.items())
+                    new_flags = curr_optimization_set - best_optimization_set
+                    removed_flags = best_optimization_set - curr_optimization_set
+                    (flag_name, value), = new_flags
+                    if flag_name not in next_optimization:
+                        next_optimization[flag_name] = value
+                        assert not removed_flags
+                    else:
+                        (flag_name2, old_value), = removed_flags
+                        assert flag_name == flag_name2
+                        if next_optimization[flag_name] == old_value:
+                            next_optimization[flag_name] = value
+            best_optimization = next_optimization
         return best_optimization
