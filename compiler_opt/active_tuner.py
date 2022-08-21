21cf1753ea814d1b3b0771dd98bbead7203c9411
Manuel Nowack <manuel.nowack@inf.ethz.ch>
2022-08-21 17:34:43 +0200

    Refactor active tuner

--- compiler_opt/active_tuner.py
+++ compiler_opt/active_tuner.py
@@ -39,6 +39,33 @@ class ActiveTuner(base_tuner.Tuner):
         return self.evaluator.evaluate(
             self.powerset.subset_to_optimization_(subset))
 
+    def fit_and_minimize_(
+            self,
+            x: np.ndarray,
+            y: np.ndarray,
+            file: TextIO = None) -> np.ndarray:
+        start = time.perf_counter()
+        self.estimator.fit(x, y)
+        end = time.perf_counter()
+        if file is not None:
+            file.write(f"Num coefs: {len(self.estimator.est.coefs)}\n")
+            file.write(f"Fit duration: {end - start} s\n")
+            file.write(f"Train score: {self.estimator.score(x, y)}\n")
+            file.write(f"Validate score: {self.estimator.score(x, y)}\n")
+
+        if isinstance(self.estimator.est, ssftapprox.SparseDSFT3Function):
+            minimize_function = ssftapprox.minimization.minimize_dsft3
+        elif isinstance(self.estimator.est, ssftapprox.SparseWHTFunction):
+            minimize_function = ssftapprox.minimization.minimize_wht
+        else:
+            raise ValueError("Unsupported estimator")
+        start = time.perf_counter()
+        min_feature, _ = minimize_function(self.estimator.est)
+        end = time.perf_counter()
+        if file is not None:
+            file.write(f"Minimize duration: {end - start} s\n")
+        return min_feature
+
     def find_best_optimization(
             self,
             budget: int,
@@ -63,29 +90,17 @@ class ActiveTuner(base_tuner.Tuner):
         if file is not None:
             file.write(f"Alpha: {self.estimator.enet_alpha}\n")
         for i in range(offline_budget, budget):
-            start = time.perf_counter()
-            self.estimator.fit(x_train, y_train)
-            end = time.perf_counter()
             if file is not None:
                 file.write("\n")
                 file.write(f"Query {i + 1}\n")
-                file.write(f"Num coefs: {len(self.estimator.est.coefs)}\n")
-                file.write(f"Fit duration: {end - start} s\n")
-                file.write(
-                    f"Train score: {self.estimator.score(x_train, y_train)}\n")
-                file.write(f"Validate score: {self.estimator.score(x, y)}\n")
-            start = time.perf_counter()
-            min_feature, _ = ssftapprox.minimization.minimize_wht(
-                self.estimator.est)
-            end = time.perf_counter()
-            if file is not None:
-                file.write(f"Minimize duration: {end - start} s\n")
+            min_feature = self.fit_and_minimize_(x_train, y_train, file)
             next_feature = min_feature.astype(x_train.dtype)
             for prev_feature in x_train:
                 if np.array_equal(prev_feature, next_feature):
                     next_feature = rng.random(
                         self.powerset.num_elements).round()
-                    file.write(f"Take random feature\n")
+                    if file is not None:
+                        file.write(f"Take random feature\n")
                     break
             x_train = np.vstack((x_train, next_feature))
             y_train = np.append(y_train, self.evaluate_subset(next_feature))
