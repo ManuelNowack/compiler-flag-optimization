780bd934e3f15fc0378a5dc79d315193d3f0dd95
Manuel Nowack <manuel.nowack@inf.ethz.ch>
2022-08-22 13:46:47 +0200

    Include train runtime in csv + lots of refactoring

--- compiler_opt/experiment.py
+++ compiler_opt/experiment.py
@@ -6,7 +6,6 @@ import pandas as pd
 
 from . import base_tuner
 from . import flag_info
-from . import simulator
 from .typing import SearchSpace
 
 
@@ -83,48 +82,55 @@ class Experiment():
         with open(f"{self.base_path_}.txt", "x") as fh:
             fh.write(f"search space size: {len(self.search_space) - 1}\n")
             fh.write(f"budget: {self.budget}\n")
-            if issubclass(self.evaluator_type, simulator.Simulator):
-                row_names = [name for tuner in self.results[0]
-                             for name in ("Default", tuner.name, "Optimal")]
-            else:
-                row_names = [name for tuner in self.results[0]
-                             for name in ("Default", tuner.name)]
+            row_names = []
+            for tuner in self.results[0]:
+                row_names.append("Default")
+                try:
+                    tuner.train_runtime
+                    row_names.append("Train")
+                except AttributeError:
+                    pass
+                row_names.append(tuner.name)
+                try:
+                    tuner.evaluator.min()
+                    row_names.append("Optimal")
+                except AttributeError:
+                    pass
             df = pd.DataFrame(index=row_names)
             for module, tuners in zip(self.modules, self.results):
-                if issubclass(self.evaluator_type, simulator.Simulator):
-                    df[module] = [
-                        runtime for tuner in tuners for runtime in (
-                            tuner.default_runtime,
-                            tuner.best_runtime,
-                            tuner.evaluator.min())]
-                else:
-                    df[module] = [
-                        runtime for tuner in tuners for runtime in (
-                            tuner.default_runtime,
-                            tuner.best_runtime)]
+                runtimes = []
                 for tuner in tuners:
+                    fh.write("\n")
+                    fh.write(f"{module} with {tuner.name}\n")
+                    # Baseline runtime
+                    runtimes.append(tuner.default_runtime)
+                    # Best runtime encountered in training data
+                    try:
+                        runtimes.append(tuner.train_runtime)
+                        speedup_train = tuner.default_runtime / tuner.train_runtime
+                        fh.write(f"speedup train: {speedup_train:.3f}\n")
+                    except AttributeError:
+                        pass
+                    # Best runtime learned by tuner
+                    runtimes.append(tuner.best_runtime)
+                    speedup = tuner.default_runtime / tuner.best_runtime
+                    fh.write(f"speedup: {speedup:.3f}\n")
+                    # Optimal runtime
+                    try:
+                        min_runtime = tuner.evaluator.min()
+                        runtimes.append(min_runtime)
+                        speedup_optimal = tuner.default_runtime / min_runtime
+                        fh.write(f"speedup optimal: {speedup_optimal:.3f}\n")
+                    except AttributeError:
+                        pass
+
+                    fh.write(f"default runtime: {tuner.default_runtime:.3e}\n")
+                    fh.write(f"best runtime: {tuner.best_runtime:.3e}\n")
                     default_flags = flag_info.optimization_to_str(
                         tuner.default_optimization, tuner.search_space)
                     best_flags = flag_info.optimization_to_str(
                         tuner.best_optimization, tuner.search_space)
-                    speedup = tuner.default_runtime / tuner.best_runtime
-                    try:
-                        speedup_train = tuner.default_runtime / tuner.train_runtime
-                    except AttributeError:
-                        speedup_train = None
-                    if issubclass(self.evaluator_type, simulator.Simulator):
-                        speedup_optimal = tuner.default_runtime / tuner.evaluator.min()
-                    else:
-                        speedup_optimal = None
-                    fh.write("\n")
-                    fh.write(f"{module} with {tuner.name}\n")
-                    fh.write(f"speedup: {speedup:.3f}\n")
-                    if speedup_train is not None:
-                        fh.write(f"speedup train: {speedup_train:.3f}\n")
-                    if speedup_optimal is not None:
-                        fh.write(f"speedup optimal: {speedup_optimal:.3f}\n")
-                    fh.write(f"default runtime: {tuner.default_runtime:.3e}\n")
-                    fh.write(f"best runtime: {tuner.best_runtime:.3e}\n")
                     fh.write(f"default flags: {default_flags}\n")
                     fh.write(f"best flags: {best_flags}\n")
+                df[module] = runtimes
         df.to_csv(f"results/{self.base_path_}.csv")
